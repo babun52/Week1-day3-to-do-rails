72be32b520c972e0c66b209e31caf6e36b537b76
James Lee <babun5209@gmail.com>
2017-02-09 16:00:02 -0800

    Add integration testing, application layout and implcit rendering, and asset pipeline and Bootstrap.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,31 +1,28 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
-  end
-
-  def show
-    @list = List.find(params[:id])
-    render :show
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "List successfully added!"
       redirect_to lists_path
     else
       render :new
     end
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
